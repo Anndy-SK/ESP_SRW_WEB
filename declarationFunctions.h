8c98ee80cd29ebd37569678c3a759f7137bd6154
AnndySK <andrejklein123@gmail.com>
2024-02-08 11:25:06 +0100

    Create declarationFunctions.h

---
+++ declarationFunctions.h
@@ -0,0 +1,8 @@
+#ifndef __HANDLE_H_
+#define __HANDLE_H_
+void handle_OnConnect();
+void handle_motorOn();
+void handle_motorOff();
+void handle_NotFound();
+String SendHTML(uint8_t motorStat);
+#endif
